ead56591c59b6b826e3e95d5e31d7ac0a3a1da85
Mahmut <mahmutt.yildiz@outlook.com>
2020-05-20 18:09:59 +0300

    Updates

---
+++ precache-manifest.826f0f3384fd5c3f49bdcedef387352d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f498990db8901a480fd7562d91930ee3",
+    "url": "/Dogs-App/index.html"
+  },
+  {
+    "revision": "f6d9380f97cd76a35b7e",
+    "url": "/Dogs-App/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "69eb3823c102b2e67e5b",
+    "url": "/Dogs-App/static/css/main.da91c133.chunk.css"
+  },
+  {
+    "revision": "f6d9380f97cd76a35b7e",
+    "url": "/Dogs-App/static/js/2.52f1d6aa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Dogs-App/static/js/2.52f1d6aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69eb3823c102b2e67e5b",
+    "url": "/Dogs-App/static/js/main.baffe093.chunk.js"
+  },
+  {
+    "revision": "0a74b544635a047338d9",
+    "url": "/Dogs-App/static/js/runtime-main.16016a2b.js"
+  }
+]);
